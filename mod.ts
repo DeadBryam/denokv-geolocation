8e525d88a3c38569f3cf6ac635df605ff669f4fb
DeadBryam <Bryamu.u@gmail.com>
2023-12-20 18:49:37 -0600

    refactor: rename server file

--- mod.ts
+++ mod.ts
@@ -1,10 +1,15 @@
 import { Hono } from "https://deno.land/x/hono@v3.11.8/mod.ts";
 import { compress, cors } from "https://deno.land/x/hono@v3.11.8/middleware.ts";
+import { ipMiddleware } from "./middlewares/ip.middleware.ts";
 
 const app = new Hono();
 
+//BASIC SETUP
 app.use(cors());
 app.use('*', compress())
+
+//MIDDLEWARES
+app.use(ipMiddleware);
 
 app.get("/", (c) => c.text("Hello Hono!"));
 
