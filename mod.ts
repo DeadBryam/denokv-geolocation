f07df7408d518f4ce0248d10982de3f796e5ae7c
DeadBryam <Bryamu.u@gmail.com>
2023-12-20 19:49:51 -0600

    refactor: add visit routes at app

--- mod.ts
+++ mod.ts
@@ -1,16 +1,20 @@
 import { Hono } from "https://deno.land/x/hono@v3.11.8/mod.ts";
 import { compress, cors } from "https://deno.land/x/hono@v3.11.8/middleware.ts";
 import { ipMiddleware } from "./middlewares/ip.middleware.ts";
+import { visitsRoutes } from "./routes/index.ts";
 
 const app = new Hono();
 
 //BASIC SETUP
 app.use(cors());
-app.use('*', compress())
+app.use("*", compress());
 
 //MIDDLEWARES
 app.use(ipMiddleware);
 
-app.get("/", (c) => c.text("Hello Hono!"));
+
+//ROUTES
+app.get("/api", (c) => c.text("Deno Deploy!"));
+app.route("/api", visitsRoutes);
 
 Deno.serve(app.fetch);
