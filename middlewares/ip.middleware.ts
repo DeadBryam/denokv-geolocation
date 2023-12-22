fc50a2e003e201e1012a687b38e1eaddff28788e
DeadBryam <Bryamu.u@gmail.com>
2023-12-21 18:05:26 -0600

    refactor: migrate middleware to oak

--- middlewares/ip.middleware.ts
+++ middlewares/ip.middleware.ts
@@ -1,13 +1,20 @@
-import { MiddlewareHandler } from "https://deno.land/x/hono@v3.11.8/mod.ts";
 import { getIPLocation } from "https://deno.land/x/ip_location@v1.0.0/mod.ts";
 
-const ipMiddleware: MiddlewareHandler = async (c, next) => {
-  const netAddr = await getIPLocation();
+import { Middleware } from "https://deno.land/x/oak@v12.6.1/mod.ts";
 
-  console.log(netAddr);
+const ipMiddleware: Middleware = async (c, next) => {
+  const ip = c.request.ip;
+  const addr = await getIPLocation(ip);
 
-  c.set("X-IP", netAddr.ip);
-
+  c.state = {
+    addr:{
+      name: addr?.country_name,
+      code: addr?.country_code?.toLowerCase(),
+      city: addr?.city,
+      status: addr.error === true ? 'nodata' : 'success'
+    }
+  };
+  
   await next();
 };
 
